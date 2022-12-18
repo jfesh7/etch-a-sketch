6a9c9de1ee0ed9782a050810d9e55878d917fae0
Josiah Fesh <josiahdfesh@gmail.com>
2022-12-17 19:40:26 -0500

    Add grid creation to web page
    The main grid structure has been implemented using CSS grid. Script
    creates a default 16x16 grid on page startup. New grids can be
    created using the text box and create grid button. Simply enter
    the size of grid you want and click create grid. Clear button has
    no added functionality as of yet.

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 /*Etch-A-Sketch program*/
 
 
-
-function createGrid() {
+function createGrid(size) {
+	clearGrid();
 	const grid = document.querySelector('div.grid-container');
-	console.log(grid);
+	for (let i = 1; i <= size; i++) {
+		for (let n = 1; n <= size; n++) {
+			const gridItem = document.createElement('div');
+			gridItem.classList.add('grid-item');
+			gridItem.style.gridRow = i;
+			gridItem.style.gridColumn = n;
+			grid.appendChild(gridItem);
+		}
+	}
 }
 
-createGrid();
+function clearGrid() {
+	const grid = document.querySelector('div.grid-container');
+	let child = grid.lastElementChild;
+	while (child) {
+		grid.removeChild(child);
+		child = grid.lastElementChild;
+	}
+}
+
+function getSize() {
+	const sizeBox = document.querySelector('#grid-size-txt');
+	let size = Number(sizeBox.value);
+	sizeBox.value = '';
+	return size;
+}
+
+
+
+//Main Program
+
+createGrid(16);
+
+const gridButton = document.querySelector('#grid-button');
+const clearButton = document.querySelector('#clear-button');
+
+gridButton.addEventListener('click', () => {
+	console.log('clicked');
+	let size = getSize();
+	console.log(size);
+	if (size) {
+		createGrid(size);
+	}
+})
+
+clearButton.addEventListener('click', () => console.log('cleared'));
