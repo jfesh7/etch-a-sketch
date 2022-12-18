a7b519aa5a6ecf7a85a73459522f5e2e76e9aa35
Josiah Fesh <josiahdfesh@gmail.com>
2022-12-17 20:40:23 -0500

    Enable drawing on grid with mouse
    The user can now draw by holding down the left mouse button and
    dragging it across the grid. The clear button is now functional
    and does what it suggests.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 /*Etch-A-Sketch program*/
 
 
+const LEFT_MOUSE_BUTTON = 1;
+const RIGHT_MOUSE_BUTTON = 2;
+
 function createGrid(size) {
-	clearGrid();
+	emptyGrid();
 	const grid = document.querySelector('div.grid-container');
 	for (let i = 1; i <= size; i++) {
 		for (let n = 1; n <= size; n++) {
 			const gridItem = document.createElement('div');
 			gridItem.classList.add('grid-item');
+			gridItem.addEventListener('mouseover', colorPixel);
+			gridItem.addEventListener('mousedown', colorPixel);
 			gridItem.style.gridRow = i;
 			gridItem.style.gridColumn = n;
 			grid.appendChild(gridItem);
@@ -15,7 +20,7 @@ function createGrid(size) {
 	}
 }
 
-function clearGrid() {
+function emptyGrid() {
 	const grid = document.querySelector('div.grid-container');
 	let child = grid.lastElementChild;
 	while (child) {
@@ -31,6 +36,19 @@ function getSize() {
 	return size;
 }
 
+function colorPixel(event) {
+	if (event.buttons === LEFT_MOUSE_BUTTON) {
+		event.target.classList.add('filled');
+	}
+}
+
+function clearGrid() {
+	const gridCells = document.querySelectorAll('div.grid-item');
+	for (let i = 0; i < gridCells.length; i++) {
+		gridCells[i].classList.remove('filled');
+	}
+}
+
 
 
 //Main Program
@@ -41,12 +59,10 @@ const gridButton = document.querySelector('#grid-button');
 const clearButton = document.querySelector('#clear-button');
 
 gridButton.addEventListener('click', () => {
-	console.log('clicked');
 	let size = getSize();
-	console.log(size);
 	if (size) {
 		createGrid(size);
 	}
 })
 
-clearButton.addEventListener('click', () => console.log('cleared'));
+clearButton.addEventListener('click', clearGrid);
